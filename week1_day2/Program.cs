9c6c17f5a3e339a0ab2cb2bb6eba03087675a88a
KaitieFox <kaitiefox@gmail.com>
2017-09-26 15:14:17 -0400

    Definitions for Operators

--- week1_day2/Program.cs
+++ week1_day2/Program.cs
@@ -29,6 +29,55 @@ namespace week1_day2
             Console.WriteLine(partialNum);
             Console.WriteLine(someNumber);
 
+            //Now let's talk about operators!
+            // ARIMETIC OPERATORS
+            // + adds two operands 
+            // - subtracts the second operand from the first
+            // * multiplies both operands
+            // / divides the first operand by second
+            // % - Modulus - divides and checks remainder
+            // ++ increments operand by one. used for ints 
+            // -- decrements operand by one. ints
+            // == checks if the values of two operands are equal.
+
+            // RELATIONAL OPERATORS
+            // if they are, returns bool true.
+            // != checks if two values are unequal.
+            // if they are unequal, returns bool true. 
+
+            int jarrydAge = 29;
+            int danielAge = 32;
+
+            Console.WriteLine(jarrydAge == danielAge);
+            Console.WriteLine(jarrydAge != danielAge);
+
+            /* MOAR RELATIONAL OPERATORS
+             * > Checks if the value of the left operand is greater than the value of the right operand.
+             * if yes, then condition is true
+             * 
+             * < Checks if the vale of the left operand is less than the value of the right operand.
+             * if yes, then condition is true.
+             * 
+             * >= checks if value of left is greater or equal to right.
+             * if yes returns bool true
+             * 
+             * <= checks if left is less or equal to right
+             * if yes return bool true
+             * 
+             */
+
+            // LOGICAL OPERATORS
+            /*
+             * && is called AND operator. If both operands are non zero, then condition comes true.
+             * if both things are things, then there's an action that we do.
+             * 
+             * || OR operator. If any of the two operands are non zero then condition comes true.
+             * if either thing is a thing, then there's an action we do.
+             * 
+             * ! NOT operator. Used to reverse the logical state of an operand. 
+             * If the condition is true, then logical NOT operator will make it false.
+             */
+
         }
     }
 }
